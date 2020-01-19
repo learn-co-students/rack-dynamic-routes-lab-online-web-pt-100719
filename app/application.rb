1d64c3e30bf715aed62cb22dd5582ea2425f6eca
nicmlu <NicoleMLucien@gmail.com>
2020-01-18 19:44:23 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -25,32 +25,4 @@ class Application
         resp.finish
     end
 end
-#       @@items.each do |item|
-#         resp.write "#{item}\n"
-#       end
-#     elsif req.path.match(/search/)
-#       search_term = req.params["q"]
-#       resp.write handle_search(search_term)
-#     elsif req.path.match(/cart/)
-#       if @@cart.size == 0
-#         resp.write "Your cart is empty"
-#       else
-#         @@cart.each do |item|
-#         resp.write "#{item}\n"
-#       end
-#     end
-#     elsif req.path.match(/add/)
-#       added_item = req.params["item"]
-#       resp.write add_item(added_item) 
-#     else
-#       resp.write "Path Not Found"
-#     end
-#     resp.finish
 
-#   def handle_search(search_term)
-#     if @@items.include?(search_term)
-#       return "#{search_term} is one of our items"
-#     else
-#       return "Couldn't find #{search_term}"
-#     end
-#   end
